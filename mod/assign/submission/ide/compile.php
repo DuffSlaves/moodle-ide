90bb8478654e0e000481a9b4a12d157c6c94878d
Phantomical <phantom@lynches.ca>
2017-01-25 13:40:53 -0500

    Filled in compile.php

--- mod/assign/submission/ide/compile.php
+++ mod/assign/submission/ide/compile.php
@@ -1,9 +1,50 @@
 <?php
-    $code = $_POST['text'];
-    $lang = $_POST['lang'];
-    //Do compiley stuff
-    $o = "test";
-    echo $o;
+    function deleteDir($dirPath) {
+        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+            $dirPath .= '/';
+        }
+        $files = glob($dirPath . '*', GLOB_MARK);
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                deleteDir($file);
+            } else {
+                unlink($file);
+            }
+        }
+        rmdir($file);
+    }
+
+    function tempdir()
+    {
+        $tempfile = tempnam(sys_get_temp_dir(), 'moodle');
+        if (file_exists($tempfile)) {
+            unlink($tempfile);
+        }
+
+        mkdir($tempfile);
+        if (is_dir($tempfile)) {
+            return $tempfile;
+        }
+    }
+
+    $object['text'] = $_POST['text'];
+    $object['lang'] = $_POST['lang'];
+    
+    $tmp = tempdir();
+
+    $json = json_encode($object);
+
+    $input = fopen($tmp + "\input", "rw");
+    fwrite($input, $json);
+    fclose($input);
+
+    exec("convert-to-js " + $tmp + "\input " + $tmp + "\output");
+
+    $js = file_get_contents($tmp + "\input", "r");
+
+    deleteDir($tmp);
+    
+    echo($js);
 
 
 
